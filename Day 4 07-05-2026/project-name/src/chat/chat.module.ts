1bf46ca14c2412621770b0fdfec8721f05fb8c80
Harini R <harinirajesh0519@gmail.com>
2026-05-21 12:10:22 +0530

    Update chat.module.ts

--- Day 4 07-05-2026/project-name/src/chat/chat.module.ts
+++ Day 4 07-05-2026/project-name/src/chat/chat.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
-import { Chat } from './entities/chat.entity';
+import { ChatService } from './chat.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Chat])
-  ],
   controllers: [ChatController],
   providers: [ChatService],
 })
